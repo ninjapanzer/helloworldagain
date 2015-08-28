09591ae0278a8878ba7ce5bd1c8d395f9c5a0791
Gary A. Newsome <garyanewsome@gmail.com>
2015-08-28 13:21:23 -0400

    HelloWorldAgain
    @#$%^!

---
+++ HelloWorldAgain.java
@@ -0,0 +1,12 @@
+package helloworldagain;
+/*
+Gary A. Newsome
+ */
+
+public class HelloWorldAgain {
+    public static void main(String[] args) {
+        System.out.println("Hello H.A.L., do you read me?");
+        System.out.println("Hello Dave, you're looking well today.");
+    }
+    
+}
