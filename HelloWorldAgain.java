fc24a9775ae9d04007f28c19e84d61f3929af854
Paul Scarrone <ninjapanzer@gmail.com>
2015-08-29 00:21:31 -0400

    A fun animated version of this conversation
    Running from netbeans will not give the animated output. You will need to do it from the command line.
    Read the output of the __Build__ button in netbeans you should see something like this
    ```
    To run this application from the command line without Ant, try:
    java -jar "/Users/samuraipanzer/NetBeansProjects/JavaApplication3/dist/JavaApplication3.jar"
    ```

--- HelloWorldAgain.java
+++ HelloWorldAgain.java
@@ -4,9 +4,21 @@ Gary A. Newsome
  */
 
 public class HelloWorldAgain {
-    public static void main(String[] args) {
-        System.out.println("Hello H.A.L., do you read me?");
-        System.out.println("Hello Dave, you're looking well today.");
+    public static void main(String[] args) throws InterruptedException {
+        String daveMessage = "Hello H.A.L., do you read me?";
+        String halMessage = "Hello Dave, you're looking well today.";
+        
+        for(int i = 0; i < daveMessage.length(); i++){
+            System.out.print(daveMessage.charAt(i));
+            Thread.sleep(100);
+        }
+        Thread.sleep(400);
+        System.out.println();
+        for(int i = 0; i < halMessage.length(); i++){
+            System.out.print(halMessage.charAt(i));
+            Thread.sleep(100);
+        }
+        System.out.println();
     }
     
 }
